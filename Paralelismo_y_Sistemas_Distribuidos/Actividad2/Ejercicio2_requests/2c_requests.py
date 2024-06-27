e1514de668178332b39be02c22c8ad66b81897c5
Pablo Sánchez Arias <45993759+Paulitos@users.noreply.github.com>
2024-06-27 20:35:03 +0200

    Add files via upload

---
+++ Paralelismo_y_Sistemas_Distribuidos/Actividad2/Ejercicio2_requests/2c_requests.py
@@ -0,0 +1,25 @@
+import requests
+import asyncio
+
+def fetch_data(url):
+    response = requests.get(url)
+    return response.json()
+
+async def analyze_market_data(urls):
+    loop = asyncio.get_running_loop()
+    async with asyncio.TaskGroup() as tg:
+        tasks = [loop.run_in_executor(None, fetch_data, url) for url in urls]
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+    return results
+
+async def main():
+    urls = [
+        "https://jsonplaceholder.typicode.com/posts/1",
+        "https://jsonplaceholder.typicode.com/posts/2",
+        "https://jsonplaceholder.typicode.com/posts/3"
+    ]
+    data = await analyze_market_data(urls)
+    print(data)
+
+if __name__ == "__main__":
+    asyncio.run(main())
